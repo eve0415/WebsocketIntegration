9c7df806a707a47cea0f02c0405ff7ffc1722d68
eve0415 <40632436+eve0415@users.noreply.github.com>
2020-10-11 22:51:22 +0900

    Refactor all codes

--- papermc/src/main/java/net/eve0415/spigot/WebsocketIntegration/websocket/EventState.java
+++ papermc/src/main/java/net/eve0415/spigot/WebsocketIntegration/websocket/EventState.java
@@ -1,8 +1,17 @@
 package net.eve0415.spigot.WebsocketIntegration.websocket;
 
 public enum EventState {
-    STARTING("STARTING"), STARTED("STARTED"), MESSAGE("MESSAGE"), COMMAND("COMMAND"), ACHIEVEMENT("ACHIEVEMENT"),
-    JOIN("JOIN"), QUIT("QUIT"), DEATH("DEATH"), STOPPING("STOPPING"), STATUS("STATUS"), LINK("LINK");
+    STARTING("STARTING"),
+    STARTED("STARTED"),
+    MESSAGE("MESSAGE"),
+    COMMAND("COMMAND"),
+    ACHIEVEMENT("ACHIEVEMENT"),
+    JOIN("JOIN"),
+    QUIT("QUIT"),
+    DEATH("DEATH"),
+    STOPPING("STOPPING"),
+    STATUS("STATUS"),
+    LINK("LINK");
 
     private final String value;
 
