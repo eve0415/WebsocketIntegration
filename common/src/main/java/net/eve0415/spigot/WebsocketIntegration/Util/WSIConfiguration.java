91574352f670bc96df68aaa812c2a424ac6b5a72
eve0415 <40632436+eve0415@users.noreply.github.com>
2020-10-14 23:30:41 +0900

    A little rewrite

--- common/src/main/java/net/eve0415/spigot/WebsocketIntegration/Util/WSIConfiguration.java
+++ common/src/main/java/net/eve0415/spigot/WebsocketIntegration/Util/WSIConfiguration.java
@@ -1,17 +1,8 @@
 package net.eve0415.spigot.WebsocketIntegration.Util;
 
-public class WSIConfiguration {
+public final class WSIConfiguration {
     private String address;
     private int port;
-
-    public WSIConfiguration() {
-
-    }
-
-    public WSIConfiguration(String address, int port) {
-        this.address = address;
-        this.port = port;
-    }
 
     public String getAddress() {
         return address;
--- common/src/main/java/net/eve0415/spigot/WebsocketIntegration/WebsocketEventHandler.java
+++ common/src/main/java/net/eve0415/spigot/WebsocketIntegration/WebsocketEventHandler.java
@@ -2,41 +2,39 @@ package net.eve0415.spigot.WebsocketIntegration;
 
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
+import net.eve0415.spigot.WebsocketIntegration.Util.WSIEventState;
 
 public class WebsocketEventHandler {
-    private WebsocketManager instance;
     private Socket socket;
 
     public WebsocketEventHandler(WebsocketManager instance, Socket socket) {
-        this.instance = instance;
         this.socket = socket;
         initWebsocketEvents();
     }
 
     private void initWebsocketEvents() {
-        socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+        socket.once(Socket.EVENT_CONNECT, new Emitter.Listener() {
             @Override
             public void call(final Object... args) {
-                if (!instance.isConnected()) {
-                    instance.getWSILogger().info("Connected");
-                    instance.isConnected(true);
-                }
+                WebsocketManager.getInstance().getWSILogger().info("Connected");
+                WebsocketManager.getInstance().isConnected(true);
+                WebsocketManager.getInstance().send(WSIEventState.STARTING, null);
             }
         });
 
         socket.on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
             @Override
             public void call(final Object... args) {
-                instance.getWSILogger().info("Disconnected: " + args[0].toString());
-                instance.isConnected(false);
+                WebsocketManager.getInstance().getWSILogger().info("Disconnected: " + args[0].toString());
+                WebsocketManager.getInstance().isConnected(false);
             }
         });
 
         socket.on(Socket.EVENT_RECONNECT, new Emitter.Listener() {
             @Override
             public void call(final Object... args) {
-                instance.getWSILogger().info("Reconnected");
-                instance.isConnected(true);
+                WebsocketManager.getInstance().getWSILogger().info("Reconnected");
+                WebsocketManager.getInstance().isConnected(true);
             }
         });
     }
